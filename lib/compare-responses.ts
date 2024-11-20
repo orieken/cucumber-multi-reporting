72a8a8cedf0564d8cf24f83d72e5249211821895
Oscar Rieken <oriekenjr@gmail.com>
2024-11-20 12:27:30 -0600

    :bug: (sample) pu

--- lib/compare-responses.ts
+++ lib/compare-responses.ts
@@ -33,3 +33,27 @@ function baseCompare<T>(actual: T, expected: T): boolean {
   // Recursively compare each key-value pair
   return areValuesEqual(actual, expected);
 }
+
+function validateApiResponse<T extends object>(response: unknown, expectedType: T): response is T {
+  return (
+    typeof response === 'object' &&
+    response !== null &&
+    response instanceof Object &&
+    Object.keys(response).every((key) => key in expectedType)
+  );
+}
+
+interface ABC {
+  name: string;
+  foo: number[];
+}
+
+async function fetchAndValidate(): Promise<boolean> {
+  const response = await fetch('your_api_endpoint');
+  const data = await response.json();
+
+  return validateApiResponse<ABC>(data, { name: '', foo: [] });
+}
+
+const f = fetchAndValidate();
+console.log(f);
